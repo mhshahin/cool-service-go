47f54b7db943668e25aa230e039422969c6ccb1a
mhshahin <mhshahin91@gmail.com>
2023-10-27 16:46:18 +0330

    Implemented and added UserRepository

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"database/sql"
+
+	userrepository "github.com/cool-service-go/repository/user_repository"
+)
+
+type Repository struct {
+	UserRepository *userrepository.UserRepository
+}
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{
+		UserRepository: userrepository.NewUserRepository(db),
+	}
+}
