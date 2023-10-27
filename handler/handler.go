e016337fdfcc1e4cb5f7108a5a546aa5c60eae74
mhshahin <mhshahin91@gmail.com>
2023-10-27 18:14:04 +0330

    Passed service to handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,12 +1,15 @@
 package handler
 
-import "github.com/cool-service-go/repository"
+import (
+	"github.com/cool-service-go/repository"
+	"github.com/cool-service-go/service"
+)
 
 type Handler struct {
 	UserHandler *UserHandler
 }
 
-func NewHandler(repo *repository.Repository) *Handler {
+func NewHandler(repo *repository.Repository, service *service.Service) *Handler {
 	return &Handler{
 		UserHandler: NewUserHandler(repo),
 	}
