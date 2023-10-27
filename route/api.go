149532ea92e6a5edbac274e6652c770b8588e75f
mhshahin <mhshahin91@gmail.com>
2023-10-27 16:46:59 +0330

    Added routes

--- route/api.go
+++ route/api.go
@@ -1,1 +1,13 @@
 package route
+
+import (
+	"github.com/cool-service-go/handler"
+	"github.com/labstack/echo/v4"
+)
+
+func InitRoutes(e *echo.Echo, handlers *handler.Handler) {
+	api := e.Group("/api")
+
+	api.GET("/users", handlers.UserHandler.GetUsers())
+	api.POST("/users", handlers.UserHandler.AddUsers())
+}
