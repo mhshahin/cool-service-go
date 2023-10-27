ed6ac6cb4de4a77cd9612702fd06f4da1c07a3f7
mhshahin <mhshahin91@gmail.com>
2023-10-28 03:07:11 +0330

    Implemented jwt middleware

---
+++ middleware/jwt_middleware.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"github.com/cool-service-go/config"
+	"github.com/cool-service-go/model"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+type JwtMiddleware struct {
+	cfg *config.AppConfig
+}
+
+func NewJwtMiddleware(cfg *config.AppConfig) *JwtMiddleware {
+	return &JwtMiddleware{
+		cfg: cfg,
+	}
+}
+
+func (jm JwtMiddleware) Middleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		tokenString := c.Request().Header.Get("Authorization")
+		if tokenString == "" {
+			return echo.ErrUnauthorized
+		}
+
+		token, err := jwt.ParseWithClaims(tokenString, &model.JwtCustomClaims{},
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(jm.cfg.JWT.Secret), nil
+			},
+		)
+		if err != nil {
+			return echo.ErrUnauthorized
+		}
+
+		claims, ok := token.Claims.(*model.JwtCustomClaims)
+		if !ok || !token.Valid {
+			return echo.ErrUnauthorized
+		}
+
+		c.Set("authenticated", true)
+		c.Set("role", claims.Role)
+
+		return next(c)
+	}
+}
