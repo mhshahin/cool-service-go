17d496429e1f70b7b1ef62c783a67e4930cc1e62
mhshahin <mhshahin91@gmail.com>
2023-10-28 03:34:12 +0330

    Removed FirstName and LastName and added Name

--- model/user.go
+++ model/user.go
@@ -1,19 +1,13 @@
 package model
 
 import (
-	"fmt"
 	"time"
 )
 
 type User struct {
 	ID        int       `json:"id,omitempty"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
+	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
-}
-
-func (u User) GetFullname() string {
-	return fmt.Sprintf("%s %s", u.FirstName, u.LastName)
 }
